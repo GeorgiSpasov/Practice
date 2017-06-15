857272ca6e3590c5e6846998629d1a26a47c43c2
Georgi Spasov <georgi.spasov@outlook.com>
2017-06-15 12:38:22 +0300

    Maze Runner

---
+++ Alpha_2017_01.MazeRunner/MazeRunner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Alpha_2017_01.MazeRunner
+{
+    class MazeRunner
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] directions = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int sumEven = 0;
+                int sumOdd = 0;
+                int asciiCode = 48;
+                string input = Console.ReadLine();
+
+                foreach (var digit in input)
+                {
+                    if ((digit - asciiCode) % 2 == 0)
+                    {
+                        sumEven += (digit - asciiCode);
+                    }
+                    else
+                    {
+                        sumOdd += (digit - asciiCode);
+                    }
+
+                    if (sumEven > sumOdd)
+                    {
+                        directions[i] = "left";
+                    }
+                    else if (sumEven < sumOdd)
+                    {
+                        directions[i] = "right";
+                    }
+                    else
+                    {
+                        directions[i] = "straight";
+                    }
+                }
+            }
+
+            foreach (string direction in directions)
+            {
+                Console.WriteLine(direction);
+            }
+        }
+    }
+}
---
+++ Alpha_2017_01.MazeRunner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Alpha_2017_01.MazeRunner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Alpha_2017_01.MazeRunner")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("badfb756-06b8-46d4-a6fe-e16d1a7070d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
