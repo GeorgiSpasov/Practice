4478bd5875c1e938ecac4779c1e9cc3e764c8dd3
Georgi Spasov <georgi.spasov@outlook.com>
2017-06-15 12:55:56 +0300

    Paper Cutter

---
+++ Alpha_2017_02.PaperCutter/PaperCutter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Alpha_2017_02.PaperCutter
+{
+    class PaperCutter
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] sheetsCapacity = new int[11];
+            for (int i = 0; i < sheetsCapacity.Length; i++)
+            {
+                sheetsCapacity[sheetsCapacity.Length - 1 - i] = (int)Math.Pow(2, i);
+            }
+
+            for (int i = 0; i < sheetsCapacity.Length; i++)
+            {
+                if (n >= sheetsCapacity[i] && sheetsCapacity[i] != 0)
+                {
+                    n -= sheetsCapacity[i];
+                    sheetsCapacity[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < sheetsCapacity.Length; i++)
+            {
+                if (sheetsCapacity[sheetsCapacity.Length - 1 - i] != 0)
+                {
+                    Console.WriteLine("A" + (sheetsCapacity.Length - 1 - i));
+                }
+            }
+        }
+    }
+}
