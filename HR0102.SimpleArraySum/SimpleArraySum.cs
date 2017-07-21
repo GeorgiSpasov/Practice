0c6be7253496fc574671da5be755db715e4f0ee0
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 00:13:38 +0300

    Simple Array Sum

---
+++ HR0102.SimpleArraySum/SimpleArraySum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0102.SimpleArraySum
+{
+    public class SimpleArraySum
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = ArraySum(n, intArray);
+            Console.WriteLine(sum);
+        }
+
+        public static int ArraySum(int n, int[] array)
+        {
+            int result = array.Sum();
+
+            return result;
+        }
+    }
+}
