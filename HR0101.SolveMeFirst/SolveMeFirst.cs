f8f5bf273584b4f6f868b241e4c5cdb61867836a
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-21 15:28:15 +0300

    Solve Me First

---
+++ HR0101.SolveMeFirst/SolveMeFirst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HR0101.SolveMeFirst
+{
+    public class SolveMeFirst
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sum = SumInts(a, b);
+            Console.WriteLine(sum);
+        }
+
+        public static int SumInts(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+    }
+}
