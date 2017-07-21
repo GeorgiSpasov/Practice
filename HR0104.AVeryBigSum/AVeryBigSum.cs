cc53b8c418ba3ef03d027d7ce8ccfdf7b7100b35
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 00:46:23 +0300

    A Very Big Sum

---
+++ HR0104.AVeryBigSum/AVeryBigSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0104.AVeryBigSum
+{
+    class AVeryBigSum
+    {
+        static long aVeryBigSum(int n, long[] ar)
+        {
+            // Complete this function
+            return ar.Sum();
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+            long result = aVeryBigSum(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
