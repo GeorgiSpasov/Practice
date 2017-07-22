517318d60e57477f72ee87256a6089ef9e970e33
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 13:02:01 +0300

    Diagonal Difference

---
+++ HR0105.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0105.DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] intArray = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+            int sumPrimary = 0;
+            int sumSecondary = 0;
+            int absDifference = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sumPrimary += intArray[i][i];
+                sumSecondary += intArray[n - 1 - i][i];
+            }
+
+            absDifference = Math.Abs(sumPrimary - sumSecondary);
+            Console.WriteLine(absDifference);
+        }
+    }
+}
