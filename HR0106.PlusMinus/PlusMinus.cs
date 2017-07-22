6dedde24614a8777b4845fa65569829fcf3570c3
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 13:19:31 +0300

    Plus Minus

---
+++ HR0106.PlusMinus/PlusMinus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0106.PlusMinus
+{
+    class PlusMinus
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int positiveCount = 0;
+            int negativeCount = 0;
+            int zeroCount = 0;
+
+            foreach (int item in intArray)
+            {
+                if (item > 0)
+                {
+                    positiveCount++;
+                }
+                else if (item < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+
+            Console.WriteLine((double)positiveCount / n);
+            Console.WriteLine((double)negativeCount / n);
+            Console.WriteLine((double)zeroCount / n);
+        }
+    }
+}
