ce65654409d45ed8f888cf3c533070ea5932e19b
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 13:30:02 +0300

    Staircase

---
+++ HR0107.Staircase/Staircase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HR0107.Staircase
+{
+    class Staircase
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string(' ', n - 1 - i) + new string('#', i + 1));
+            }
+        }
+    }
+}
