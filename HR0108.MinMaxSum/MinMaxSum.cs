d9d6adbc3d5d5db1a57704c839ce4164ee96b21c
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 13:53:10 +0300

    Min Max Sum

---
+++ HR0108.MinMaxSum/MinMaxSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0108.MinMaxSum
+{
+    class MinMaxSum
+    {
+        static void Main(string[] args)
+        {
+            long[] intArray = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long minSum = intArray.Sum() - intArray.Max();
+            long maxSum = intArray.Sum() - intArray.Min();
+
+            Console.WriteLine(minSum + " " + maxSum);
+        }
+    }
+}
