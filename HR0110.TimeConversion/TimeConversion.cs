a82c590dc26a700a6298c31c0e4ffbf51eee5d81
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 15:48:38 +0300

    Time Conversion

---
+++ HR0110.TimeConversion/TimeConversion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HR0110.TimeConversion
+{
+    class TimeConversion
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string convertedTime = TimeConverter(input);
+            Console.WriteLine(convertedTime);
+        }
+
+        static string TimeConverter(string input)
+        {
+            string result;
+
+            if (input[input.Length - 2] == 'A')
+            {
+                if (input.Substring(0, 2) == "12")
+                {
+                    result = "00" + (input.Substring(2, input.Length - 4));
+                }
+                else
+                {
+                    result = input.Substring(0, input.Length - 2);
+                }
+            }
+            else
+            {
+                if (input.Substring(0, 2) == "12")
+                {
+                    result = input.Substring(0, input.Length - 2);
+                }
+                else
+                {
+                    result = (int.Parse(input.Substring(0, 2)) + 12) + (input.Substring(2, input.Length - 4));
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
