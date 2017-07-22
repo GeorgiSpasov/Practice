e6b418835b1fd79f0208d64130307de18d32258d
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-22 14:44:08 +0300

    Birthday Cake Candles

---
+++ HR0109.BirthdayCakeCandles/BirthdayCakeCandles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0109.BirthdayCakeCandles
+{
+    class BirthdayCakeCandles
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] candles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int result = 0;
+
+            int maxHight = candles.Max();
+            foreach (int candle in candles)
+            {
+                if (candle == maxHight)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
