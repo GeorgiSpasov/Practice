d70a6619e41bce51138d339f2633f712e13b445d
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-24 21:49:31 +0300

    Kangaroo

---
+++ HR0113.Kangaroo/Kangaroo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0113.Kangaroo
+{
+    class Kangaroo
+    {
+
+        static string KangarooCheck(int start1, int rate1, int start2, int rate2)
+        {
+            string result = "";
+            if (rate1 == rate2 || (start1 > start2 && rate1 > rate2) || (start1 < start2 && rate1 < rate2))
+            {
+                result = "NO";
+            }
+            else
+            {
+                if ((start1 - start2) % (rate2 - rate1) == 0)
+                {
+                    result = "YES";
+                }
+                else
+                {
+                    result = "NO";
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start1 = input[0];
+            int rate1 = input[1];
+            int start2 = input[2];
+            int rate2 = input[3];
+            string result = KangarooCheck(start1, rate1, start2, rate2);
+            Console.WriteLine(result);
+        }
+    }
+}
