2107598649267fe501aae314c1bf8534b5cce2fb
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-26 21:24:36 +0300

    Birthday Chocolate

---
+++ HR0116.BirthdayChocolate/BirthdayChocolate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace HR0116.BirthdayChocolate
+{
+    class BirthdayChocolate
+    {
+        static int BarsCombinations(int[] bars, int day, int month)
+        {
+            int combinations = 0;
+
+            for (int i = 0; i < bars.Length - month + 1; i++)
+            {
+                int sum = 0;
+                for (int m = 0; m < month; m++)
+                {
+                    sum += bars[i + m];
+                }
+                if (sum == day)
+                {
+                    combinations++;
+                }
+            }
+
+            return combinations;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] bars = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] dm = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int day = dm[0];
+            int month = dm[1];
+
+            int combinations = BarsCombinations(bars, day, month);
+            Console.WriteLine(combinations);
+        }
+    }
+}
