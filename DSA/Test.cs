e9a7dfe42dc8b31ea4c657c8242b15f1674c8f8f
Georgi Spasov <georgi.spasov@outlook.com>
2017-08-25 12:08:05 +0300

    LinkedList

---
+++ DSA/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<string> linkedList = new LinkedList<string>();
+            LinkedListNode<string> first = new LinkedListNode<string>("first");
+            linkedList.AddFirst(first);
+            linkedList.AddAfter(linkedList.Find("first"), "after first");
+            linkedList.AddLast("last");
+            int counter = 0;
+            var result = linkedList.Select(n => $"{counter++}. {n}").ToList();
+
+            Console.WriteLine(string.Join(", ",result));
+
+            SortedSet<string> set = new SortedSet<string>();
+
+        }
+    }
+}
